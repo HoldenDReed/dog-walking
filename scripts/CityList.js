5ea16d8df365cb0e90ca9b37b148db97d691f744
Holden D Reed <holdendreed@hotmail.com>
2022-09-08 04:03:51 -0500

    Adding to git

---
+++ scripts/CityList.js
@@ -0,0 +1,16 @@
+import { getCities } from "./database.js"
+
+const cities = getCities()
+
+export const CityList = () => {
+    let citiesHTML = "<ol>"
+
+    for (const city of cities) {
+        citiesHTML += `<li>${city.name}</li>`
+    }
+
+    citiesHTML += "</ol>"
+
+    return citiesHTML
+}
+
---
+++ scripts/Walkers.js
@@ -0,0 +1,31 @@
+import { getCitiesForWalker, getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("walker")) {
+            const [,walkerId] = itemClicked.id.split("--")
+            const walkerCities = getCitiesForWalker(parseInt(walkerId))
+            const walkerCitiesString = walkerCities.join(", ")
+            for (const walker of walkers) {
+                if (walker.id === parseInt(walkerId)) {
+                    window.alert(`${walker.name} services ${walkerCitiesString}`)
+                }
+            }
+        }
+    }
+)
+export const Walkers = () => {
+    let walkerHTML = "<ul>"
+
+    for (const walker of walkers) {
+        walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`
+    }
+
+    walkerHTML += "</ul>"
+    return walkerHTML
+}
+
